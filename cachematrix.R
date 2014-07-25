87c9b30f9b6a79715c3b151043f599cd2fd17bc4
LOVEWARRIOR <2643651327@qq.com>
2014-07-25 15:53:02 +0800

    revise2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(origin_matrix = matrix()) {
+        #As you see , I wrote a human readable program
+        #inverse means an inverse of a matrix
+        #origin_matrix is the matrix you what to computes with
+        #setinverse and getinverse have similar function like setmean and getmean
+        
+        inverse <- NULL
+        set <- function(y) {
+                origin_matrix <<- y
+                inverse <<- NULL
+        }
+        get <- function() origin_matrix
+        setinverse <- function(new_inverse) inverse <<- new_inverse
+        getinverse <- function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}on
 
-}
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+        #inverse is the answer we what , the inverse of a particular matrix
+        # x is the cache where the program stores a spectial vector
+        #created in last function (including inverse)
+        #check if inverse has already in the x
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        matrix<- x$get()
+        inverse<- solve(matrix,...)
+        x$setinverse(inverse)
+        inverse
 }
